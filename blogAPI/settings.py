32943377535ee2f0c3cb091f85958c87525b15e3
Chimjiokem <chimjiokemanya@gmail.com>
2025-01-30 02:15:11 -0800

    Changed debug to true

--- blogAPI/settings.py
+++ blogAPI/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+blgirfs&0t5k^!uy35^tm@=b4!(d^0x4kwurwciov4*46*b3$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
